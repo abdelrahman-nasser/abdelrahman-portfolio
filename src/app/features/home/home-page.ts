042decfc227fa4c807057708053de4f96ec22e9a
abdelrahman-nasser <ar.nasser56@yahoo.com>
2026-09-03 15:22:58 +0300

    feat: add homepage experience preview

--- src/app/features/home/home-page.ts
+++ src/app/features/home/home-page.ts
@@ -3,11 +3,12 @@ import { RouterLink } from '@angular/router';
 
 import { portfolioProfile } from '../../content';
 import { PageContainer } from '../../layout/page-container/page-container';
+import { ExperiencePreview } from './experience-preview/experience-preview';
 import { ProfessionalSnapshot } from './professional-snapshot/professional-snapshot';
 import { SelectedWork } from './selected-work/selected-work';
 
 @Component({
-  imports: [PageContainer, ProfessionalSnapshot, RouterLink, SelectedWork],
+  imports: [ExperiencePreview, PageContainer, ProfessionalSnapshot, RouterLink, SelectedWork],
   selector: 'app-home-page',
   styleUrl: './home-page.scss',
   templateUrl: './home-page.html',
