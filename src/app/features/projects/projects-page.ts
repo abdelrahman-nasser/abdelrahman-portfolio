dc5f27deaa5e3d6fa3a279f8bc2f5e69008c8028
abdelrahman-nasser <ar.nasser56@yahoo.com>
2026-09-03 23:10:23 +0300

    feat: implement projects index page

--- src/app/features/projects/projects-page.ts
+++ src/app/features/projects/projects-page.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { portfolioProjects } from '../../content';
 import { PageContainer } from '../../layout/page-container/page-container';
+import { ProjectCard } from '../../shared/components/project-card/project-card';
 
 @Component({
-  imports: [PageContainer],
+  imports: [PageContainer, ProjectCard],
   selector: 'app-projects-page',
+  styleUrl: './projects-page.scss',
   templateUrl: './projects-page.html',
 })
-export class ProjectsPage {}
+export class ProjectsPage {
+  protected readonly projects = portfolioProjects;
+}
